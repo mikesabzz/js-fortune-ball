d39da3f5c13c7171e310a854551f6fa0fd20a89b
Michael Sabzevari <michaelsabzevari@gmail.com>
2019-11-03 21:03:01 -0500

    temporarily finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-fortune-ball
+
+![](https://lh3.googleusercontent.com/QK0AePZwxWzoWzkhhouONiAHdsKynfHIiT9G1vnEmxvuTFORxIsXl4PS2fPMgnbl4P0.png)
--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 const randomAnswers = [
     'Yes',
-    'No',
-    'Maybe',
-    'Figure out what you want!',
-    'Set your intention',
     'It is certain',
     'Definately',
     'Without a doubt',
-    'Ask again later',
+    'You may rely on it',
+    'Outlook looks good',
+    'No',
     'No means no',
     'Absolutely not',
+    'Very doubtful',
+    'Don\'t count on it',
+    'Doesn\'t look good',
+    'Maybe',
+    'Ask again later',
     'Better not tell you now',
     'Not promising',
     'Perhaps',
     'Cannot predict now'
 ];
-
 function fortuneTeller(randomAnswers){
     let answers = randomAnswers[Math.floor(Math.random() * randomAnswers.length)]
-        document.querySelector('.answers').innerHTML = "loading";
+        document.querySelector('.answers').innerText = "loading"
     setTimeout(function(){
         document.querySelector('.answers').innerHTML = answers;
     }, 2000);
